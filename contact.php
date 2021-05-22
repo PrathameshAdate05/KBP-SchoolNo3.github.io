625067403a351bdd2bdd441422328f6898b4a040
Prathamesh Adate <78736071+PrathameshAdate05@users.noreply.github.com>
2021-05-22 09:35:49 +0530

    webiste done

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+
+$fname = $_POST['first_name'];
+$laname = $_POST['last_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$msg = $_POST['msg'];
+$to_email = "prathameshadate9@gmail.com";
+$subject = "Message from $fname $laname";
+$body = "Phone :$phone  Email :$email  Message :$msg";
+$headers = "From: karmveerbhauraopatil123@gmail.com";
+
+if (mail($to_email, $subject, $body, $headers)) {
+    
+	header('Location: contact.html');
+	
+	echo '<script>alert("We get your response..")</script>';
+	
+} else {
+    echo '<script>alert("Something get wrong try again..!!")</script>';
+}
+?>
